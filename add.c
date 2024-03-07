364b43f5ed1ce3dbf626826d78df3e4eba5f922c
kramoth <kramoth@udm.ac.mu>
2024-03-07 09:37:49 +0400

    adding a gitignore file and add.c file

---
+++ add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+
+	int a,b;
+	printf("Premier nombre a additionner ?:\n");
+	scanf("%d",&a);
+	printf("Deuxieme nombre a additionner ?:\n");
+	scanf("%d",&b);
+	printf("%d + %d = %d\n",a,b,a+b);
+}
